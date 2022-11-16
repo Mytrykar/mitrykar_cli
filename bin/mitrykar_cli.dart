ebd23d6f93a224af7d3fb02859a023d6635db62e
Mytrykar <ipushka1996@gmail.com>
2022-11-16 02:45:13 +0200

    first commit

---
+++ bin/mitrykar_cli.dart
@@ -0,0 +1,5 @@
+import 'package:mitrykar_cli/mitrykar_cli.dart' as mitrykar_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mitrykar_cli.calculate()}!');
+}
---
+++ test/mitrykar_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:mitrykar_cli/mitrykar_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
