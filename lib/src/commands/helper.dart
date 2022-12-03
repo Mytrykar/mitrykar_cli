546760d0c418dc549337b2a3207cc0dd2954c92f
Mytrykar <ipushka1996@gmail.com>
2022-12-03 16:04:28 +0200

    k

--- lib/src/commands/helper.dart
+++ lib/src/commands/helper.dart
@@ -6,18 +6,15 @@ import 'package:pubspec_yaml/pubspec_yaml.dart';
 
 String get dirPath => Directory.current.path;
 
-class Helper with _CliDirectory,_Routers, _CliYaml{
-  
-
+class Helper with _CliDirectory, _Routers, _CliYaml {
   static String get projectName {
     PubspecYaml yaml =
         File("/$dirPath/pubspec.yaml").readAsStringSync().toPubspecYaml();
     return yaml.name;
   }
-
 }
 
-mixin _CliYaml{
+mixin _CliYaml {
   static Future<bool> checkProjectType(ProjectType projectType) async {
     if (!_CliDirectory.checkCreateInCli()) return false;
     final yaml = await File("$dirPath/.cli/cli.yaml").readAsLines();
@@ -25,32 +22,42 @@ mixin _CliYaml{
   }
 }
 
-mixin _CliDirectory{
+mixin _CliDirectory {
   static checkCreateInCli() => Directory("$dirPath/.cli").existsSync();
 
   static void createCliDir() => Directory("$dirPath/.cli").createSync();
 }
 
-mixin _Routers{
-   static List<String> get allPatches {
+class _Routers {
+  static Map<String, dynamic>? _routersMap;
+
+  static void readFile() {
     final routers = File("$dirPath/.cli/routers.json").readAsStringSync();
-    final collection = JsonDecoder().convert(routers) as Map;
+    _routersMap = JsonDecoder().convert(routers) as Map<String, dynamic>;
+  }
+
+  static List<String> get allPatches {
+    readFile();
+
     List<String> allPatches = [];
-    void mappedJson(Map<String, dynamic> map, String parent) {
-      for (var element in map.entries) {
-        if (element.value is String) {
-          allPatches.add(parent + element.value);
-        } else {
-          mappedJson(element.value as Map<String, dynamic>, parent + );
-        }
-      }
-    }
 
-
-    mappedJson(collection as Map<String, dynamic>, "");
+    _mappedJson(_routersMap!, "");
 
     return allPatches;
   }
+
+  static _mappedJson(Map<String, dynamic> map, String parent) {
+    for (var element in map.entries) {
+      if (element.value is String) {
+        allPatches.add(parent + element.value);
+      } else {
+        _mappedJson(element.value as Map<String, dynamic>,
+            parent + _searchPath(element.key));
+      }
+    }
+  }
+
+  static _searchPath(String screenName) {}
 }
 
 void main() {
