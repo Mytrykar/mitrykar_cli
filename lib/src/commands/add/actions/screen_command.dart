23790f48a15f3262cf05331d1ec1c49a38b5efd0
mytrykar <ipushka1996@gmail.com>
2022-12-04 16:19:04 +0200

    d

--- lib/src/commands/add/actions/screen_command.dart
+++ lib/src/commands/add/actions/screen_command.dart
@@ -2,9 +2,7 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
-import 'package:change_case/change_case.dart';
 import 'package:mason_logger/mason_logger.dart';
-import 'package:project_cli/src/commands/helper.dart';
 import 'package:project_cli/src/utils.dart';
 
 void main(List<String> args) {
@@ -63,62 +61,5 @@ Replase ROUTE?: example: /screen2/:params as String or /screen2:
     return logger.prompt(
         "Create name new Screen, !!!screen name must be unique!!!, example: Sing In",
         defaultValue: null);
-  }
-}
-
-class ScreenParameters {
-  final List<String> routes;
-  final String screenName;
-  final File routerFile;
-  final Directory? screensDir;
-  final Directory routesDir;
-  final String projectName;
-
-  ScreenParameters(this.projectName,
-      {required this.routes,
-      required this.routerFile,
-      required this.screenName,
-      this.screensDir,
-      required this.routesDir});
-
-  factory ScreenParameters.named(List<String> routes, String screenName) {
-    final _d = Directory.current.path;
-    final File routerFile = File("$_d/lib/app/router/router.dart");
-    Directory? screensDir;
-    final Directory routesDir = Directory("$_d/lib/app/router/routes");
-
-    if (!ScreenParameters.exist(screenName)) {
-      screensDir = Directory("$_d/lib/ui/screens");
-    }
-    if (!routerFile.existsSync() || !routesDir.existsSync()) {
-      throw "Creation is impossible";
-    }
-    return ScreenParameters(Helper.projectName,
-        routerFile: routerFile,
-        routes: routes,
-        routesDir: routesDir,
-        screenName: screenName,
-        screensDir: screensDir);
-  }
-
-  static bool exist(String screenName) {
-    final _d = Directory.current.path;
-    return File("$_d/lib/ui/screens/${screenName.toSnakeCase()}").existsSync();
-  }
-}
-
-abstract class $Gen {
-  Future<void> run();
-}
-
-class $GenNewScreen implements $Gen {
-  final ScreenParameters genParameters;
-
-  $GenNewScreen(this.genParameters);
-
-  @override
-  Future<void> run() async {
-    if (genParameters.screensDir == null) return;
-    throw UnimplementedError();
   }
 }
